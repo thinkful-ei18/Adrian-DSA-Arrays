0a738918ad98a44bd3e156794898d5987fdf85dd
Adrian <adriantoddross@gmail.com>
2018-04-04 16:34:18 -0400

    stub maxsum

---
+++ maxsum.js
@@ -0,0 +1,8 @@
+'use strict';
+
+
+// Max sum in the array
+// You are given an array containing positive and negative integers. Write an algorithm which will find the largest sum in a continuous sequence.
+
+// Input: [4,6,-3,5,-2,1]
+// Output: 12
