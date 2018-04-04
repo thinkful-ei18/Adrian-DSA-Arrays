db37ee212e96cfb0a30158d2270f82c6017937e4
Adrian <adriantoddross@gmail.com>
2018-04-04 14:16:24 -0400

    test methods again

--- playground.js
+++ playground.js
@@ -2,13 +2,12 @@
 
 const Array = require('./array');
 
-let testArray = new Array();
 
 function sandbox() {
+  Array.SIZE_RATIO = 3;
+  let testArray = new Array();
+
   testArray.push(5);
-  testArray.push(10);
-  console.log(testArray);
-  testArray.remove(1);
   console.log(testArray);
 }
 
