178e7ddb8e0785034635c2a75a18f16820e6e984
Adrian <adriantoddross@gmail.com>
2018-04-04 12:42:20 -0400

    add sandbox to playground :-)

--- playground.js
+++ playground.js
@@ -4,8 +4,12 @@ const Array = require('./array');
 
 let testArray = new Array();
 
-testArray.push(5);
-testArray.push(10);
-console.log(testArray);
-testArray.remove(1);
-console.log(testArray);
+function sandbox() {
+  testArray.push(5);
+  testArray.push(10);
+  console.log(testArray);
+  testArray.remove(1);
+  console.log(testArray);
+}
+
+sandbox();
